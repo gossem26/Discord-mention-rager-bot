ee65d92a6258a2af308ff9f33aad3693d5a432f0
Ely Lavie <ely.lavie@gmail.com>
2022-04-11 14:49:22 +0300

    only read from responses.txt when modified, and put contents in variable responses

--- mentionrager.py
+++ mentionrager.py
@@ -1,9 +1,10 @@
-import discord
+import discord, random, asyncio, os
 from discord.ext import commands
-import random
-import asyncio
+
 
 client = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
+response_mtime = 0
+responses = []
 
 @client.event
 async def on_ready():
@@ -11,12 +12,17 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+	global response_mtime, responses
 	if(message.mention_everyone == True or str(client.user.id) in message.content):
-		async with message.channel.typing():
-			await asyncio.sleep(4)
+		if(os.stat("responses.txt").st_mtime != response_mtime):
+			print("responses updated")
+			response_mtime = os.stat("responses.txt").st_mtime
 			with open("responses.txt", 'r') as r:
 				responses = r.readlines()
-				await message.channel.send(responses[random.randrange(len(responses))])
+		async with message.channel.typing():
+			await asyncio.sleep(4)
+			await message.channel.send(responses[random.randrange(len(responses))])
 
 with open("token.txt", 'r') as t:
 	client.run(t.read())
+
